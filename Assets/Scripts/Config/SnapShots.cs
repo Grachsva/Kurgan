548030404aa5679215980bd09ffef78e91929c09
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-08-23 09:19:31 +0300

    открытие конфиг. сцена по 2 сек задержке

--- Assets/Scripts/Config/SnapShots.cs
+++ Assets/Scripts/Config/SnapShots.cs
@@ -3,133 +3,141 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SnapShots : MonoBehaviour
+namespace Snapshots
 {
-    public Camera configCamera; // Камера для конфигурационной сцены
-    public Camera otherCamera;  // Другая камера
-    public Button makeShapshot;
-    public Button clearShapshot;
-    public GameObject speedSlider;
-    public GameObject speedText;
-    public string saveFileName = "snapshots.json"; // Имя файла для сохранения
-
-    public List<GameObject> SnapShotobjects = new List<GameObject>();
-
-    private List<CameraData> savedSnapshots = new List<CameraData>();
-
-    private void OnEnable()
+    public class SnapShots : MonoBehaviour
     {
-        LoadFromFile(); // Загрузка данных из файла при старте
-        LoadSnapshots(); // Восстановление объектов из загруженных данных
-    }
+        public Camera configCamera; // Камера для конфигурационной сцены
+        public Camera otherCamera;  // Другая камера
+        public Button makeShapshot;
+        public Button clearShapshot;
+        public GameObject speedSlider;
+        public GameObject speedText;
+        public string saveFileName = "snapshots.json"; // Имя файла для сохранения
 
-    public void EnableConfigScene()
-    {
-        // Включаем конфигурационную камеру и выключаем другую
-        configCamera.gameObject.SetActive(!configCamera.gameObject.activeInHierarchy);
-        otherCamera.gameObject.SetActive(!otherCamera.gameObject.activeInHierarchy);
-        makeShapshot.gameObject.SetActive(!makeShapshot.gameObject.activeInHierarchy);
-        clearShapshot.gameObject.SetActive(!clearShapshot.gameObject.activeInHierarchy);
+        public List<GameObject> SnapShotobjects = new List<GameObject>();
 
-        speedSlider.gameObject.SetActive(!speedSlider.gameObject.activeInHierarchy);
-        speedText.gameObject.SetActive(!speedText.gameObject.activeInHierarchy);
+        private List<CameraData> savedSnapshots = new List<CameraData>();
 
-        for (int i = 0; i < SnapShotobjects.Count; i++)
+        private void OnEnable()
         {
-            SnapShotobjects[i].SetActive(!SnapShotobjects[i].activeInHierarchy);
-        }
-    }
-
-    public void SaveCurrentCameraState()
-    {
-        CameraData snapshot = new CameraData
-        {
-            position = configCamera.transform.position,
-            rotation = configCamera.transform.rotation.eulerAngles,
-        };
-        savedSnapshots.Add(snapshot);
-
-        // Сохранение в JSON файл
-        SaveToFile();
-        MakeSnapShotObj(snapshot);
-    }
-
-    private void MakeSnapShotObj(CameraData snapshot)
-    {
-        var snaphotPrefab = Resources.Load<GameObject>("Config/snapshot");
-        GameObject snaphotObj = Instantiate(snaphotPrefab, snapshot.position, Quaternion.Euler(snapshot.rotation));
-        // Получаем компонент Text в дочернем Canvas и устанавливаем порядковый номер
-        var textComponent = snaphotObj.GetComponentInChildren<Text>();
-        if (textComponent != null)
-        {
-            textComponent.text = (SnapShotobjects.Count + 1).ToString();
+            LoadFromFile(); // Загрузка данных из файла при старте
+            LoadSnapshots(); // Восстановление объектов из загруженных данных
         }
 
-        SnapShotobjects.Add(snaphotObj);
-    }
-
-    public void ClearSnapshots()
-    {
-        savedSnapshots.Clear();
-        SaveToFile(); // Сохраняем пустой список в файл
-        Debug.Log("JSON файл очищен.");
-
-        for (int i = 0; i < SnapShotobjects.Count; i++)
+        private void Start()
         {
-            DestroyImmediate(SnapShotobjects[i], true);
+            ButtonConfig.e_OnButtonConfig += EnableConfigScene;
         }
-        SnapShotobjects.Clear(); // Очищаем список объектов
-    }
 
-    private void SaveToFile()
-    {
-        // Использование папки StreamingAssets
-        string filePath = Path.Combine(Application.streamingAssetsPath, saveFileName);
-        string json = JsonUtility.ToJson(new CameraDataList { snapshots = savedSnapshots }, true);
-        File.WriteAllText(filePath, json);
-        Debug.Log("Данные сохранены в " + filePath);
-    }
-
-    public void LoadFromFile()
-    {
-        // Использование папки StreamingAssets
-        string filePath = Path.Combine(Application.streamingAssetsPath, saveFileName);
-        if (File.Exists(filePath))
+        public void EnableConfigScene()
         {
-            string json = File.ReadAllText(filePath);
-            CameraDataList dataList = JsonUtility.FromJson<CameraDataList>(json);
-            savedSnapshots = dataList.snapshots;
+            // Включаем конфигурационную камеру и выключаем другую
+            configCamera.gameObject.SetActive(!configCamera.gameObject.activeInHierarchy);
+            otherCamera.gameObject.SetActive(!otherCamera.gameObject.activeInHierarchy);
+            makeShapshot.gameObject.SetActive(!makeShapshot.gameObject.activeInHierarchy);
+            clearShapshot.gameObject.SetActive(!clearShapshot.gameObject.activeInHierarchy);
+
+            speedSlider.gameObject.SetActive(!speedSlider.gameObject.activeInHierarchy);
+            speedText.gameObject.SetActive(!speedText.gameObject.activeInHierarchy);
+
+            for (int i = 0; i < SnapShotobjects.Count; i++)
+            {
+                SnapShotobjects[i].SetActive(!SnapShotobjects[i].activeInHierarchy);
+            }
         }
-        else
-        {
-            Debug.LogWarning("Файл не найден: " + filePath);
-        }
-    }
 
-    private void LoadSnapshots()
-    {
-        SnapShotobjects.Clear(); // Очищаем список объектов перед загрузкой
-        foreach (var snapshot in savedSnapshots)
+        public void SaveCurrentCameraState()
         {
+            CameraData snapshot = new CameraData
+            {
+                position = configCamera.transform.position,
+                rotation = configCamera.transform.rotation.eulerAngles,
+            };
+            savedSnapshots.Add(snapshot);
+
+            // Сохранение в JSON файл
+            SaveToFile();
             MakeSnapShotObj(snapshot);
         }
 
-        for (int i = 0; i < SnapShotobjects.Count; i++)
+        private void MakeSnapShotObj(CameraData snapshot)
         {
-            SnapShotobjects[i].SetActive(!SnapShotobjects[i].activeInHierarchy);
+            var snaphotPrefab = Resources.Load<GameObject>("Config/snapshot");
+            GameObject snaphotObj = Instantiate(snaphotPrefab, snapshot.position, Quaternion.Euler(snapshot.rotation));
+            // Получаем компонент Text в дочернем Canvas и устанавливаем порядковый номер
+            var textComponent = snaphotObj.GetComponentInChildren<Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = (SnapShotobjects.Count + 1).ToString();
+            }
+
+            SnapShotobjects.Add(snaphotObj);
         }
-    }
 
-    [System.Serializable]
-    public class CameraData
-    {
-        public Vector3 position;
-        public Vector3 rotation;
-    }
+        public void ClearSnapshots()
+        {
+            savedSnapshots.Clear();
+            SaveToFile(); // Сохраняем пустой список в файл
+            Debug.Log("JSON файл очищен.");
 
-    [System.Serializable]
-    public class CameraDataList
-    {
-        public List<CameraData> snapshots;
+            for (int i = 0; i < SnapShotobjects.Count; i++)
+            {
+                DestroyImmediate(SnapShotobjects[i], true);
+            }
+            SnapShotobjects.Clear(); // Очищаем список объектов
+        }
+
+        private void SaveToFile()
+        {
+            // Использование папки StreamingAssets
+            string filePath = Path.Combine(Application.streamingAssetsPath, saveFileName);
+            string json = JsonUtility.ToJson(new CameraDataList { snapshots = savedSnapshots }, true);
+            File.WriteAllText(filePath, json);
+            Debug.Log("Данные сохранены в " + filePath);
+        }
+
+        public void LoadFromFile()
+        {
+            // Использование папки StreamingAssets
+            string filePath = Path.Combine(Application.streamingAssetsPath, saveFileName);
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                CameraDataList dataList = JsonUtility.FromJson<CameraDataList>(json);
+                savedSnapshots = dataList.snapshots;
+            }
+            else
+            {
+                Debug.LogWarning("Файл не найден: " + filePath);
+            }
+        }
+
+        private void LoadSnapshots()
+        {
+            SnapShotobjects.Clear(); // Очищаем список объектов перед загрузкой
+            foreach (var snapshot in savedSnapshots)
+            {
+                MakeSnapShotObj(snapshot);
+            }
+
+            for (int i = 0; i < SnapShotobjects.Count; i++)
+            {
+                SnapShotobjects[i].SetActive(!SnapShotobjects[i].activeInHierarchy);
+            }
+        }
+
+        [System.Serializable]
+        public class CameraData
+        {
+            public Vector3 position;
+            public Vector3 rotation;
+        }
+
+        [System.Serializable]
+        public class CameraDataList
+        {
+            public List<CameraData> snapshots;
+        }
     }
 }
