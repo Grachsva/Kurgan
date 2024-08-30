60144517a05a53dd54029a3ba39f76602debc33f
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-08-30 14:05:54 +0300

    перевод графики

--- Assets/Scripts/Settings/FPS_limit.cs
+++ Assets/Scripts/Settings/FPS_limit.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
 
 
 public class FPS_limit : MonoBehaviour 
@@ -10,6 +12,13 @@ public class FPS_limit : MonoBehaviour
     {
         QualitySettings.vSyncCount = 1;
         Application.targetFrameRate = 60;
+        HDRenderPipeline refToPipeline = (HDRenderPipeline)RenderPipelineManager.currentPipeline;
+        //Invoke("MyResetFunction", 1.0f / 300.0f);
         Debug.Log(Application.targetFrameRate);
+    }
+
+    private void MyResetFunction()
+    {
+        HDRenderPipeline refToPipeline = (HDRenderPipeline)RenderPipelineManager.currentPipeline;
     }
 }
