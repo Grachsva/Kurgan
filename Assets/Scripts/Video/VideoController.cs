b168773fc4c05f9b6a187f689cb3cc191a4e3dcf
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-09-10 19:28:39 +0300

    sum

--- Assets/Scripts/Video/VideoController.cs
+++ Assets/Scripts/Video/VideoController.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class VideoController : MonoBehaviour
 {
     [SerializeField] private GameObject _playButton;
 
+    private VideoPlayer _player;
+
     private void OnEnable()
     {
         _playButton.SetActive(true);
+        _player = GetComponent<VideoPlayer>();
+        _player.loopPointReached += EndReached;
     }
 
     public void SetFrame(int frame)
     {
-        
-        GetComponent<VideoPlayer>().frame = frame;
+        _player.frame = frame;
+        //GetComponent<VideoPlayer>().frame = frame;
     }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        _playButton.SetActive(true);
+        _player.frame = 100;
+    }
+
 }
